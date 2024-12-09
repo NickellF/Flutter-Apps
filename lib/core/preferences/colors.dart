78477f45415059ae8671d802293f4d0762a5f8bc
ntesseract <ibrakim0169@gmail.com>
2024-12-09 14:47:23 +0700

    nambah color tako

--- lib/core/preferences/colors.dart
+++ lib/core/preferences/colors.dart
@@ -3,48 +3,65 @@ import 'package:flutter/material.dart';
 class AppColors {
   AppColors._();
 
+  static const tako = MaterialColor(0xFFFF722C, {
+    50: Color(0xFFFFEBEE), // Lightest shade
+    100: Color(0xFFFFC1B6),
+    200: Color(0xFFFFA07A),
+    300: Color(0xFFFF7D4D),
+    400: Color(0xFFFF5A2D),
+    500: Color(0xFFFF722C), // Main color
+    600: Color(0xFFEA5E24),
+    700: Color(0xFFD05420),
+    800: Color(0xFFB74A1C),
+    900: Color(0xFF9B3D16), // Darkest shade
+  });
+
   static const green = MaterialColor(0xff2DBD78, {
     200: Color(0xff5ED99E),
     400: Color(0xff35CF85),
     600: Color(0xff28A96B),
     800: Color(0xff1E8051),
   });
-     static const yellow = MaterialColor(0xffFFD101, {
+
+  static const yellow = MaterialColor(0xffFFD101, {
     200: Color(0xffFFDF4C),
     400: Color(0xffFFD51A),
     600: Color(0xffE5B400),
     800: Color(0xffB28300),
-    });
-     static const orange = MaterialColor(0xffFF722C, {
+  });
+
+  static const orange = MaterialColor(0xffFF722C, {
     200: Color(0xff5ED99E),
     400: Color(0xff35CF85),
     600: Color(0xff28A96B),
     800: Color(0xff1E8051),
-    });
-     static const red = MaterialColor(0xffEB4755, {
+  });
+
+  static const red = MaterialColor(0xffEB4755, {
     200: Color(0xffF28C95),
     400: Color(0xffED5E6A),
     600: Color(0xffE83040),
     800: Color(0xffCF1726),
-    });
-     static const blue = MaterialColor(0xff476BEB, {
+  });
+
+  static const blue = MaterialColor(0xff476BEB, {
     200: Color(0xff8CA3F2),
     400: Color(0xff5E7EED),
     600: Color(0xff3059E8),
     800: Color(0xff173FCF),
-    });
+  });
 
-     static const black = MaterialColor(0xff666666, {
+  static const black = MaterialColor(0xff666666, {
     200: Color(0xff999999),
     400: Color(0xff808080),
     600: Color(0xff4D4D4D),
     800: Color(0xff333333),
-    });
+  });
 
-     static const white = MaterialColor(0xffE0E0E0, {
+  static const white = MaterialColor(0xffE0E0E0, {
     200: Color(0xffFAFAFA),
     400: Color(0xffF5F5F5),
     600: Color(0xffCCCCCC),
     800: Color(0xffB8B8B8),
   });
-  }
+}
